3e7cdfea9bf186964e32b1429f26b4c46da8f52b
Advexp Components <components@advexp.net>
2018-12-29 00:32:30 +0300

    fix compilation errors

--- Samples/LocalSettings/Windows/Properties/AssemblyInfo.cs
+++ Samples/LocalSettings/Windows/Properties/AssemblyInfo.cs
@@ -1,18 +1,17 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// General assembly properties
 [assembly: AssemblyTitle("Sample.LocalSettings.Windows")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sample.LocalSettings.Windows")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("Sample.LocalSettings.Windows")]
+[assembly: AssemblyCompany("Advexp")]
+[assembly: AssemblyProduct("Advexp.Settings.Samples")]
+[assembly: AssemblyCopyright("Alexey Ivakin")]
+[assembly: AssemblyTrademark("Advexp.Settings")]
+
+// Assembly version information
+[assembly: AssemblyVersion("1.0.0.0")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -21,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("96d1a9fb-df00-4edc-b0fe-3b8eda2a7966")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
